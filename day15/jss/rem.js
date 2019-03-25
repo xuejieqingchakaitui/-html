0a6a2aff5b2c2f11178e738a9cb7016008cef172
xuejieqingchakaitui <354643363@qq.com>
2019-03-25 17:38:22 +0800

    珠峰学习的css课程

---
+++ day15/jss/rem.js
@@ -0,0 +1,2 @@
+var winW = document.documentElement.clientWidth;
+document.documentElement.style.fontSize     = winW / 15 + 'px';
---
+++ day15course/js/index15.js
@@ -0,0 +1,5 @@
+/**
+ * Created by liuweixing on 2019/3/8.
+ */
+var winW = document.documentElement.clientWidth;
+document.documentElement.style.fontSize     = winW / 7.5 + 'px';
